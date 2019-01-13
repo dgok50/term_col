c239fce0c2e0d80f7adde355a236e46b4d57df40
dgok50 <dgok50@a1mc.ru>
2019-01-13 09:16:12 +0300

    ...

--- multicast_lib.c
+++ multicast_lib.c
@@ -6,16 +6,15 @@
 #include <unistd.h> 
 #include <stdlib.h>
 
-int send_multicast(char *broadcast_addr, int *port_addr, char *message)
-//int init_sock()
+int send_multicast(char *broadcast_addr, int *port_addr, char *message) 
 {
    int sock = -1;
    //char message[] = "retyergtr";
    struct sockaddr_in addr;
-   int addrlen, cnt, mes_size=strlen(message)*sizeof(char)+2, optval = 1; //Исправить говнокод
+   int addrlen, cnt, mes_size=strlen(message)*sizeof(char)+2, optval = 1;
    struct ip_mreq mreq;
    //char message_raw[sizeof(message)+2];
-   char message_raw[mes_size];
+   char message_raw[mes_size]; //Исправить говнокод, выдилить память нормально
    /* set up socket */
    if(sock <= 0){
     sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -36,22 +35,18 @@ int send_multicast(char *broadcast_addr, int *port_addr, char *message)
    addr.sin_addr.s_addr = inet_addr(broadcast_addr);
    //addr.sin_addr.s_addr = inet_addr(EXAMPLE_GROUP);
    //printf("sock:%d\n", sock);
-   //while (1) {
-	 sprintf(message_raw, "%c%s%c",25,message,01);
-	 //printf("size %d, sending: %s\n", mes_size, message_raw);
-	 cnt = sendto(sock, message_raw, mes_size, 0, (struct sockaddr *) &addr, addrlen);
-	 //printf("\nerror:%d",cnt);
-	 if (cnt < 0) {
-		//perror("sendto1");
-		return -2;
-	 }
-	 //sleep(5);
-	 //}
- close(sock);
- return 0;
+  
+   sprintf(message_raw, "%c%s%c",25,message,01);
+   //printf("size %d, sending: %s\n", mes_size, message_raw);
+   cnt = sendto(sock, message_raw, mes_size, 0, (struct sockaddr *) &addr, addrlen);
+   if (cnt < 0) {
+     return -2;
+   }
+   close(sock);
+   return 0;
 }
 /*
-int send_multicast(uint32_t broadcast_addr, uint16_t port_addr, char *message)
+int recive_multicast(uint32_t broadcast_addr, uint16_t port_addr, char *message, int *m_len)
 {
    struct sockaddr_in addr;
    int addrlen, sock, cnt;
@@ -93,6 +88,6 @@ int send_multicast(uint32_t broadcast_addr, uint16_t port_addr, char *message)
 		break;
 	 }
 	 printf("%s: message = \"%s\"\n", inet_ntoa(addr.sin_addr), message);
-		}
 	}
+}
 	*/
--- verling_kuip.sh
+++ verling_kuip.sh
@@ -6,5 +6,5 @@ mkdir $HOME/term_col/log/kuip-valgrind/
 valgrind --leak-check=no --track-origins=yes --gen-suppressions=all \
   --read-var-info=yes \
   --log-file=$HOME/term_col/log/kuip-valgrind/%p.log \
-  --time-stamp=yes --trace-children=yes ./kuip \
+  --time-stamp=yes --trace-children=yes ./kuip -k \
   2>&1 | tee $HOME/term_col/log/kuip-valgrind/kuip.log
